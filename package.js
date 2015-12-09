fcb6fda8d66bb07193c63e2783512e1a97d57940
zVictor <zvictor@users.noreply.github.com>
2015-12-09 12:10:53 +0100

    brings package to zvictor's namespace

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   summary: 'Relations for Meteor Astronomy',
   version: '1.0.0',
-  name: 'jagi:astronomy-relations',
-  git: 'https://github.com/jagi/meteor-astronomy-relations.git'
+  name: 'zvictor:astronomy-relations',
+  git: 'https://github.com/zvictor/meteor-astronomy-relations.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('jagi:astronomy@1.2.6');
+  api.use('zvictor:astronomy@1.2.6');
   api.use('underscore');
 
-  api.imply('jagi:astronomy');
+  api.imply('zvictor:astronomy');
 
   // Module.
   api.addFiles([
